ddb6325b73d1735e3f351e7d68e261a64b70ad56
Quinn Wilson <Lunky@midworld.org>
2023-06-07 11:24:19 +0000

    add dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('demonstrates that the test runner is working', ()=> {
+  const a = 2;
+  const b = 3;
+  const total = a + b;
+  expect(total).toBe(5);
+})
